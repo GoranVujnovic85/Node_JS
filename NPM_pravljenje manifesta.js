a4e524e2880624a3c06963ff65fc76c583903fcc
Lenovo <goranvujnovic85@gmail.com>
2024-10-15 06:03:15 +0200

    NPM, pravljenje manifesta

---
+++ NPM_pravljenje manifesta.js
@@ -0,0 +1,5 @@
+// [ Ctrl ] + [ ~ ]
+// npm -v
+// npm init
+// npm i goran.demo-app --> ovako se instalira neki paket, u ovom slucaju necemo nista instalirati jer ovaj paket goran.demo-app ne postoji
+// npm i mysql2 + enter --> ovo je instalirano
